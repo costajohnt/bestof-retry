93927e667963cfbb48a0b33c2dda1738bed453d8
John Costa <johncosta@Johns-MacBook-Pro.local>
2015-12-09 17:43:15 -0800

    initial commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,13 @@
 class User < ActiveRecord::Base
 
+  
+
   has_secure_password
   # note: has_secure_password automatically adds validations
   # for presence of password, password length < 72 characters,
   # and password_confirmation
 
-  validates :password, length: { minimum: 6 }, on: :create
+  validates :password, length: { minimum: 3 }, on: :create
 
   validates :email,
     presence: true,
