4a3233beefd01ba090b4aaf26ee457a3f54ffc9d
John Costa <johncosta@johns-mbp.attlocal.net>
2015-12-10 00:52:34 -0800

    got auth, got posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,45 @@
 class PostsController < ApplicationController
+	
 	def index
 		@posts = Post.all
 		render :index
 	end
+
+	def new
+		@post = Post.new
+		render :new
+	end
+
+	def create
+		@posts = Post.all
+		@post = Post.create(post_params)
+		if @post.save
+			redirect_to @post
+		end
+	end
+
+	def show
+		@post = Post.find(params[:id])
+		render :show
+	end
+
+	def destroy
+	    # save the id parameter
+	    id = params[:id]
+	    # find the post to delete by id
+	    @post = Post.find(id)
+	    # destroy the post
+	    @post.destroy
+	    # redirect to creatures index
+	    redirect_to root_path
+	    # ^ same as redirect_to "/creatures"
+	end
+
+	private
+
+	def post_params
+		params.require(:post).permit(:title)
+	end
+
+end
+
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,12 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :integer          not null, primary key
+#  title      :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Post < ActiveRecord::Base
 end
