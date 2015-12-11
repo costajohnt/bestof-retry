ca79c7589b97cff7bba48aae7bb7da58a94a1369
John Costa <johncosta@Johns-MacBook-Pro.local>
2015-12-11 12:55:50 -0800

    search for posts works

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,12 @@ class PostsController < ApplicationController
 	def index
 		@posts = Post.all
 		render :index
+
+		if params[:search]
+		     @posts = Post.search(params[:search]).order("created_at DESC")
+		   else
+		     @posts = Post.order("created_at DESC")
+		   end
 	end
 
 	def new
